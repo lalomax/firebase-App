7506212cca9bf416115d1aa7b131462152e89368
Lalomax <lalomax365@gmail.com>
2023-08-03 18:41:13 -0400

    Connected to firebase

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://fir-app-5f71e-default-rtdb.firebaseio.com/"
@@ -11,12 +11,37 @@ const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
-const shoppingList = document.getElementById("shopping-list")
-
+const shoppingListEl = document.getElementById("shopping-list")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    
     push(shoppingListInDB, inputValue)
-    console.log(inputValue)
-    inputFieldEl.value = ""
-    shoppingList.innerHTML += `<li>${inputValue}</li>`
+    
+    clearInputFieldEl()
+
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.entries(snapshot.val())
+    
+    clearShoppingListEl()
+    
+    for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+        let currentKey = currentItem[0]
+        let currentValue = currentItem[1]
+        appendItemToShoppingListEl(currentValue)
+    }
+})
+
+function clearShoppingListEl () {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
